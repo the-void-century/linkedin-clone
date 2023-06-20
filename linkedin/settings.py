1f39e46510428e86b381dba48c90816feced14a3
the-void-century <utsdhall@gmail.com>
2023-06-20 17:53:12 +0530

    procfile

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 AUTH_USER_MODEL = 'user.User'
 
 # Password validation
