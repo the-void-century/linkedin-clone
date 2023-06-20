06d59e6593e1f686d3edecfb61c4a86462a58552
the-void-century <utsdhall@gmail.com>
2023-06-20 18:34:36 +0530

    ooo

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'linkedin.wsgi.application'
 
-
+CSRF_TRUSTED_ORIGINS=["https://linkedin-clone-production.up.railway.app"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
